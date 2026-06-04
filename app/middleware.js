656a3037d0a9a73a9ea0c2a30bb0ffe0c7145c99
akiaki0901 <akiakiunco@gmail.com>
2026-06-04 10:42:17 +0900

    こっちがセキュリティコミットだった

---
+++ app/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    const auth = request.headers.get("authorization");
+
+    const username = process.env.BASIC_AUTH_USER;
+    const password = process.env.BASIC_AUTH_PASSWORD;
+
+    const valid =
+        auth ===
+        "Basic " +
+            Buffer.from(`${username}:${password}`).toString("base64");
+
+    if (!valid) {
+        return new Response("Authentication required", {
+            status: 401,
+            headers: {
+                "WWW-Authenticate": 'Basic realm="Secure Area"',
+            },
+        });
+    }
+
+    return NextResponse.next();
+}
--- components/Pagelayout.jsx
+++ components/Pagelayout.jsx
@@ -1,12 +1,45 @@
+"use client";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
+import styles from "./mail.module.css";
 
 export default function Pagelayout({children}){
+    /*const [isRock, setIsRock] = useState(localStorage.getItem("develope-pass-broken")!=="true");
+    const [input, setInput] = useState("");
+   
+
+    const passcheck = ()=>{
+        if(input==="0901-develope-mode"){
+            localStorage.setItem("develope-pass-broken","true");
+            setIsRock(false);
+        }
+        else{
+            alert("passwordが違います。");
+        }
+    }*/
+
     return(
-        <div>
-            <Header/>
-            <>{children}</>
-            <Footer/>
+       
+        <div> 
+            {/*{isRock && 
+            <div>
+                <h1>DevelopeMode Input Password</h1>
+                <input
+                className={styles.input}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e)=>{if(e.key==="Enter")passcheck();}}
+                //onBlur={()=>setMail({name:input,tell:mail.tell,mail:mail.mail,address:mail.address,content:mail.content,})}
+                />
+            </div>}*/}
+            {/*{!isRock &&*/} 
+            <div>
+                <Header/>
+                <>{children}</>
+                <Footer/>
+            </div>{/*}*/}
+
         </div>
     );
 }
